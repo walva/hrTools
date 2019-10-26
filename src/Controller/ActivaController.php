f6620f2fdb3c51f98483cbd72eee7fc9c5512ef5
Benjamin Ellis <ellis.benjamin@outlook.com>
2019-10-26 13:53:09 +0200

    implement activa

---
+++ src/Controller/ActivaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActivaController extends AbstractController
+{
+    /**
+     * @Route("/activa", name="activa")
+     */
+    public function index(Request $request)
+    {
+        $form = $this->createFormBuilder([
+            'date'  => new \DateTime(),
+            'month' => 36,
+        ])
+            ->add('date', DateType::class, [
+                'html5'  => true,
+                'widget' => 'single_text',
+            ])
+            ->add('month', NumberType::class, [
+                'help' => 'Le nombre de mois à ajouter',
+            ])
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        $result = null;
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \DateTime $date */
+            $date = $form->get('date')->getData();
+            $month = $form->get('month')->getData();
+            $endDate = clone $date;
+            $endDate->add(new \DateInterval("P{$month}M"));
+            $request = [
+                'computed' => $endDate
+            ];
+        }
+
+        return $this->render('activa/index.html.twig', [
+            'controller_name' => 'ActivaController',
+            'form'            => $form->createView(),
+            'result'          => $request,
+        ]);
+    }
+}
